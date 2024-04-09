15edc891a4cf6fa07f316ecbde640c58110407a0
Miguel Mier <miguel.mier@babelgroup.com>
2024-04-09 12:08:47 +0200

    Doc inicial de model

--- src/main/java/com/helloworld/salaries/model/Employee.java
+++ src/main/java/com/helloworld/salaries/model/Employee.java
@@ -1,6 +1,7 @@
 package com.helloworld.salaries.model;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,17 +14,28 @@ import java.util.Date;
 public class Employee {
 
     @Schema(description = "Código del empleado")
+    @NotNull(message = "El código del empleado no puede ser null")
     private String codEmpleado;
 
     @Schema(description = "Nombre del empleado")
+    @NotNull(message = "El nombre del empleado no puede ser null")
+    @Size(min = 3, max = 50, message
+            = "El nombre debe ser entre 3 y 50 caracteres")
     private String nombreEmpleado;
 
     @Schema(description = "Fecha de inicio de trabajo")
+    @PastOrPresent
     private Date fechaInicio;
 
     @Schema(description = "Fecha de baja de trabajo")
+    @PastOrPresent
     private Date fechaBaja;
 
     @Schema(description = "Código de la oficina asociada")
+    @Positive(message = "Si no tiene oficina, el valor debe ser null")
     private int office;
+
+    @Schema(description = "Categoría de empleado, puede ser GN, GE o CO")
+    @NotNull(message = "La categoría del empleado no puede ser null")
+    private String categoria;
 }
--- src/main/java/com/helloworld/salaries/model/Office.java
+++ src/main/java/com/helloworld/salaries/model/Office.java
@@ -1,6 +1,9 @@
 package com.helloworld.salaries.model;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,21 +14,31 @@ import lombok.NoArgsConstructor;
 public class Office {
 
     @Schema(description = "Código de oficina asociada")
+    @NotNull(message = "El id de oficina no puede ser null")
     private int idOffice;
 
     @Schema(description = "Nombre de la oficina")
+    @NotNull(message = "El nombre de oficina no puede ser null")
     private String nameOffice;
 
     @Schema(description = "Ciudad donde se ubica la oficina")
+    @NotNull(message = "La ciudad de la oficina no puede ser null")
     private String city;
 
     @Schema(description = "Código postal donde se ubica la oficina")
+    @NotNull(message = "El código postal de la oficina no puede ser null")
+    @Min(value = 1)
+    @Max(value = 99999)
     private String postalCode;
 
     @Schema(description = "Código del país donde se ubica la oficina")
+    @NotNull(message = "El código del país de la oficina no puede ser null")
     private String countryCode;
 
     @Schema(description = "Indica si la oficina es Central (1) o si no (0)")
+    @NotNull(message = "Si no es headquarter, debe de ser 0, no null")
+    @Min(value = 0)
+    @Max(value = 1)
     private int headquarter;
 
     @Schema(description = "Continente donde se ubica la oficina")
